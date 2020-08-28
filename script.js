488248ae141d2df09adc6b4616204690a6396066
scrabill <shannon.crabill@gmail.com>
2020-08-28 14:53:14 -0400

    Adds script for checking form input and returning true/false if it's a palindrome

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
 
     let input = document.querySelector("#input") 
     let submit = document.querySelector("#submit") 
 
     submit.addEventListener('click', (event) => {
         event.preventDefault()
-        almostPalindrome(input.value)
+        isPalindrome(input.value)
         input.value = ''
     });
     
@@ -16,41 +15,26 @@ function reverseString(string) {
     return string.split("").reverse().join("")
 }
 
-function almostPalindrome(string) {
+function isPalindrome(string) {
 
     let reversedString = reverseString(string)
 
     if (string == reversedString) {
-        return printAnswer(true)
-    }
-
-    let i = 0
-    let missedChar = 0
-
-    console.log(`string: ${string}, rString: ${reversedString}`)
-
-    while (i < string.length) {
-        console.log(string[i] + ":" + reversedString[i])
-        if (string[i] != reversedString[i]) {  // if they don't match
-            missedChar += 1 
-            console.log(missedChar) 
-        }
-    i++
-    }
-
-    // return missedChar == 1 || missedChar == 0
-
-    console.log(missedChar)
-
-    if (missedChar <= 1) {
-        return printAnswer(true)
+        return printAnswer(string, true)
     } else {
-        printAnswer(false)
+        return printAnswer(string, false)
     }
 
 }
 
-function printAnswer(answer) {
+function printAnswer(string, answer) {
     let result = document.querySelector("#result")
-    result.innerText = answer
+    let output = `${answer}: ${string} is `
+    
+    if (answer == true) {
+        output += "a palindrome"
+    } else {
+        output += "not a palindrome"
+    }
+    result.innerText = output
 }
